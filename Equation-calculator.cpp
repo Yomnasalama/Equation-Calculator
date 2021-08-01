bcfbfee6268b943f73c6fa279158b98197bccabe
Yomnasalama <64813394+Yomnasalama@users.noreply.github.com>
2021-08-01 19:57:33 +0200

    Update Equation-calculator.cpp

--- Equation-calculator.cpp
+++ Equation-calculator.cpp
@@ -1,56 +1,56 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-stack<char> st;
 
-    int precedence(char c){
-        if(c == '^')
-            return 3;
-        else if(c == '/' || c == '*')
-            return 2;
-        else if(c == '+' || c == '-')
-            return 1;
+int prec(char c) {
+    if(c == '^')
+        return 3;
+    else if(c == '/' || c=='*')
+        return 2;
+    else if(c == '+' || c == '-')
+        return 1;
+    else
         return -1;
-    }
+}
 
-//Convert Infix Expression to Postfix Expression
-    string infix_postfix(string s){
-        string res;
-        for(int i = 0; i < s.length(); i++){
-            if(isdigit(s[i])){
-                res += '#';
-                while(isdigit(s[i]) || s[i] == '.'){
-                    res+= s[i];
-                    i++;
-                }
+string infix_postfix(string s) {
+
+    stack<char> st;
+    string res;
+
+    for(int i = 0; i < s.length(); i++) {
+
+        if(isdigit(s[i])){
+            res += '#';
+            while(isdigit(s[i]) || s[i] == '.'){
+                res +=s[i];
+                i++;
             }
-            if(s[i] == '(')
-                st.push(s[i]);
-            else if(s[i] == ')'){
-                while(st.top() != '(')
-                {
-                    res += st.top();
-                    st.pop();
-                }
+        }
+        if(s[i]== '(')
+            st.push('(');
+        else if(s[i] == ')') {
+            while(st.top() != '(')
+            {
+                res += st.top();
                 st.pop();
             }
-            else{
-                while(precedence(s[i]) <= precedence(st.top()) && st.size() != 0){
-                    res += st.top();
-                    st.pop();
-                }
-                st.push(s[i]);
-            }
-        }
-
-        while(st.size() != 0){
-            res += st.top();
             st.pop();
         }
-        return res;
+        else {
+            while(st.size() != 0 && prec(s[i]) <= prec(st.top())) {
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while(st.size() != 0) {
+        res += st.top();
+        st.pop();
     }
 
-
+    return res;
+}
 
 float evaluate_postfix(string expression)
 {
